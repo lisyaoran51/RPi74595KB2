a7bf9f9a2bdd90b97f69595959f4f192106146dd
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-08-29 00:23:05 +0800

    no message

--- wave_player.c
+++ wave_player.c
@@ -238,11 +238,11 @@ void Audio_playFile_Cut(snd_pcm_t *handle, wavedata_t *pWaveData)
 	aPiece.pData = &(pWaveData->pData[0 * 32 * (SAMPLE_RATE / 100)]);
 	aPiece.bufNum = 32 * (SAMPLE_RATE / 100);
 	printf("1\n");
-	//pthread_create(&t1, NULL, Audio_playFile_Piece, &aPiece ); // 建立子執行緒
+	pthread_create(&t1, NULL, Audio_playFile_Piece, &aPiece ); // 建立子執行緒
 	
-	usleep(10000*1000);
+	usleep(10000);
 	printf("2\n");
-	aPiece.pData = &(pWaveData->pData[1 * 32 * (SAMPLE_RATE / 100)]);
+	aPiece.pData = &(pWaveData->pData) + 1 * 32 * (SAMPLE_RATE / 100));
 	pthread_create(&t2, NULL, Audio_playFile_Piece, &aPiece ); // 建立子執行緒
 	
 	return;
