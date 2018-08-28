af2ff105aafd0222b0b80820899378698e67ac3d
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-08-29 02:07:01 +0800

    no message

--- wave_player.c
+++ wave_player.c
@@ -318,6 +318,6 @@ void Audio_playFile_Piece(AudioPiece* aPiece){
 	// snd_pcm_t *handle, short *buf, int bufNum
 	
 	// Write data and play sound (blocking)
-	snd_pcm_sframes_t frames = snd_pcm_writei(aPiece->handle, aPiece->pData, aPiece->bufNum);
+	//snd_pcm_sframes_t frames = snd_pcm_writei(aPiece->handle, aPiece->pData, aPiece->bufNum);
 	
 }
