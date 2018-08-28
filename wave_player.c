ef0bb42bf8e7049551dfceb7c3b483210daa489a
TsaiJiaYu-PC\TsaiJiaYu <lisyaoran51@hotmail.com>
2018-08-28 22:59:53 +0800

    no message

--- wave_player.c
+++ wave_player.c
@@ -153,7 +153,7 @@ void Audio_readWaveFileIntoMemory(char *fileName, wavedata_t *pWaveStruct)
 		exit(EXIT_FAILURE);
 	}
 	
-	unsigned char stuff8[2];
+	unsigned char* stuff8;
 	for(int i = 0; i < pWaveStruct->numSamples; i++){
 		stuff8 = (unsigned char*)&(pWaveStruct->pData[i]);
 		printf("%d%d ", stuff8[0], stuff8[1]);
